628a6de0ed9153f8ccbf052a0ccbadf8e3cd0cea
gabriellaperich <gabriellaperichguimaraes@gmail.com>
2025-12-07 14:42:46 -0300

    collect authors

--- extract/extract/settings.py
+++ extract/extract/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "extract.spiders"
 
 ADDONS = {}
 
-
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "extract (+http://www.yourdomain.com)"
 
--- extract/extract/spiders/amazon.py
+++ extract/extract/spiders/amazon.py
@@ -4,7 +4,12 @@ import scrapy
 class AmazonSpider(scrapy.Spider):
     name = "amazon"
     allowed_domains = ["www.amazon.com.br"]
-    start_urls = ["https://www.amazon.com.br/Livros/b/?ie=UTF8"]
+    start_urls = ["https://www.amazon.com.br/s?i=stripbooks&rh=n%3A6740748011%2Cp_36%3A-2200%2Cp_n_feature_nine_browse-bin%3A8529758011%2Cp_n_condition-type%3A13862762011&dc&qid=1765039756&rnid=13862761011&ref=sr_nr_p_n_condition-type_1&ds=v1%3AzM0bfKZhUGrxmjDqR%2BjeG44Ecn1kvzp4NOZrNlcRAqI"]
 
     def parse(self, response):
-        pass
+        products = response.css('div.puisg-col-inner')
+
+        for product in products:
+            yield {
+                'author': product.xpath('.//span[contains(text(), "por")]/following-sibling::span[1]/text()').get()
+            }
